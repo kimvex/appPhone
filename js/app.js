4dbbcb004744f7b299f69caf49b4936dbec41e67
Benjamin <chomi.numb.94@gmail.com>
2015-12-07 01:22:28 -0600

    Agregamos el proyecto

---
+++ js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+	function conectar(e){
+		var ip = document.getElementById('ip').value;
+		console.log(ip)
+		/*$.get('http://' + ip,function(data){
+			document.getElementById('action').innerHTML = data;
+		});*/
+		document.getElementById('action').innerHTML = '<object class="servervid" data="http://'+ ip +'" type="text/html"></object>';
+		e.preventDefault();
+	}
+
+	document.getElementById('boton').addEventListener('click',conectar);
+});
